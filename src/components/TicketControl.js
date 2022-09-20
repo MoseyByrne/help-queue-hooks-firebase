25aa019bc8fd312976eb0d428e8d9272a9d426ed
MoseyByrne <mosey.b.byrne@gmail.com>
2022-09-20 11:12:13 -0700

    add firestore
    Co-authored-by: Peter Vang <vang_peter06@yahoo.com>
    Co-authored-by: Zhibin Liang  <ifthereisoneday@gmail.com>

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -3,24 +3,38 @@ import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
 import EditTicketForm from './EditTicketForm';
 import TicketDetail from './TicketDetail';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import db from './../firebase.js';
+import { collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc } from "firebase/firestore";
 
 function TicketControl() {
 
   const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
   const [mainTicketList, setMainTicketList] = useState([]);
   const [selectedTicket, setSelectedTicket]= useState(null);
-    const [editing, setEditing] = useState(false);
+  const [editing, setEditing] = useState(false);
+  const [error, setError] = useState(null);
 
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     formVisibleOnPage: false,
-  //     mainTicketList: [],
-  //     selectedTicket: null,
-  //     editing: false
-  //   };
-  // }
+    useEffect(() => {
+      const unSubscribe = onSnapshot(collection(db, "tickets"), (collectionSnapshot) => {
+        const tickets = [];
+        collectionSnapshot.forEach((doc) => {
+          tickets.push({
+            names: doc.data().names, 
+            location: doc.data().location,
+            issue: doc.data().issue,
+            id: doc.id
+          });
+        });
+        setMainTicketList(tickets);
+      },
+      (error)=> {
+        setError(error.message);
+      }
+      );
+      return () => unSubscribe();
+    }, []);
+
 
   const handleClick = () => {
     if (selectedTicket != null) {
@@ -33,38 +47,37 @@ function TicketControl() {
     }
   
 
-  const handleDeletingTicket = (id) => {
-    const newMainTicketList = mainTicketList.filter(ticket => ticket.id !== id);
-    setMainTicketList(newMainTicketList);
+  const handleDeletingTicket = async (id) => {
+    await deleteDoc(doc(db,"tickets", id));
     setSelectedTicket(null);
-    // this.setState({
-    //   mainTicketList: newMainTicketList,
-    //   selectedTicket: null
-    // });
+   
   }
 
   const handleEditClick = () => {
     setEditing(true);
   }
 
-  const handleEditingTicketInList = (ticketToEdit) => {
-    const editedMainTicketList = mainTicketList
-      .filter(ticket => ticket.id !== selectedTicket.id)
-      .concat(ticketToEdit);
-      setMainTicketList(editedMainTicketList);
+  const handleEditingTicketInList = async (ticketToEdit) => {
+    const ticketRef = doc(db, "ticket", ticketToEdit.id);
+    await updateDoc(ticketRef, ticketToEdit);
       setEditing(false);
       setSelectedTicket(null);
-    // this.setState({
-    //   mainTicketList: editedMainTicketList,
-    //   editing: false,
-    //   selectedTicket: null
-    // });
   }
 
-  const handleAddingNewTicketToList = (newTicket) => {
-    const newMainTicketList = mainTicketList.concat(newTicket);
-    setMainTicketList(newMainTicketList);
-    setFormVisibleOnPage(false)
+  // The collection() function allows us to specify a collection within our firestore database. This function takes two arguments: the Firestore database instance, and the name of our collection. This function returns a CollectionReference object
+
+  // The addDoc() function allows us to add a new document to a specified collection. This function takes two arguments: a collection reference and the data to be added to the new document.
+
+  // handleAddingNewTicketToList easier way to read
+  // const handleAddingNewTicketToList = async (newTicketData) => {
+  //   const collectionRef = collection(db, "tickets");
+  //   await addDoc(collectionRef, newTicketData);
+  //   setFormVisibleOnPage(false);
+  // }
+
+  const handleAddingNewTicketToList = async (newTicketData) => {
+    await addDoc(collection(db, "ticket"), newTicketData);
+    setFormVisibleOnPage(false);
   }
 
   const handleChangingSelectedTicket = (id) => {
@@ -74,7 +87,9 @@ function TicketControl() {
   
     let currentlyVisibleState = null;
     let buttonText = null; 
-    if ( editing ) {      
+    if (error) {
+      currentlyVisibleState = <p>There was an error: {error}</p>
+    } else if ( editing ) {      
       currentlyVisibleState = <EditTicketForm ticket = {selectedTicket} onEditTicket = {handleEditingTicketInList} />
       buttonText = "Return to Ticket List";
     } else if (selectedTicket != null) {
@@ -94,7 +109,8 @@ function TicketControl() {
     return (
       <React.Fragment>
         {currentlyVisibleState}
-        <button onClick={handleClick}>{buttonText}</button> 
+        {error ? null : <button onClick=
+        {handleClick}>{buttonText}</button>} 
       </React.Fragment>
     );
   }
