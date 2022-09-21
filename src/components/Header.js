e2df1ac78317ec8ef893fdafb11b74e029d7c52b
MoseyByrne <mosey.b.byrne@gmail.com>
2022-09-21 15:44:00 -0700

    add styled components
    Co-authored-by: Peter Vang <vang_peter06@yahoo.com>
    Co-authored-by: Zhibin Liang  <ifthereisoneday@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components"
+
+const StyledWrapper = styled.section`
+  background-color: orange;
+`;
+
+const HelpQueueHeader = styled.h1`
+  font-size: 24px;
+  text-align: center;
+  color: white;
+  background-color: purple;
+`;
 
 function Header() {
   return (
-    <React.Fragment>
-      <h1>Help Queue</h1>
+    // <React.Fragment> don't need this anymore because our code is wrapped in new stylized component
+    <StyledWrapper>
+      <HelpQueueHeader>Help Queue</HelpQueueHeader>
       <ul>
         <li>
           <Link to="/">Home</Link>
@@ -13,7 +26,8 @@ function Header() {
           <Link to="/sign-in">Sign In</Link>
         </li>
       </ul>
-    </React.Fragment>
+      </StyledWrapper>
+      // </React.Fragment>
   );
 }
 
